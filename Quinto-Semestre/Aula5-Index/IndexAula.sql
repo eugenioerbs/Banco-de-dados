e0dc3885e95fae9beb44eb779bb5b85433f40786
Eugênio Victor Erbs <eugenio.job1@gmail.com>
2026-03-27 18:01:57 -0300

    Update IndexAula.sql

--- Quinto-Semestre/Aula5-Index/IndexAula.sql
+++ Quinto-Semestre/Aula5-Index/IndexAula.sql
@@ -23,9 +23,9 @@ DESC grande_usuario;
 EXPLAIN 
 SELECT gu.nm_grande_usuario, lg.nm_logradouro_comp, ba.nm_bairro, lo.nm_localidade, gu.sg_uf
 FROM grande_usuario gu
-JOIN localidade lo ON gu.cd_localidade = lo.cd_localidade
-JOIN bairro ba ON gu.cd_bairro = ba.cd_bairro
-JOIN logradouro lg ON gu.cd_logradouro = lg.cd_logradouro
+JOIN localidade lo ON lo.cd_localidade = gu.cd_localidade
+JOIN bairro ba ON ba.cd_bairro = gu.cd_bairro 
+JOIN logradouro lg ON lg.cd_logradouro = gu.cd_logradouro
 WHERE lo.nm_localidade = 'blumenau';
 
 -- plano de execução e estudar sobre explorar ao máximo essa base de dados com criação de indexes e como eles funcionam
